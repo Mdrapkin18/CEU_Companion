00a23dc12c7cc6094381f85b97b729ddc8abac6e
Mdrapkin18 <91030553+Mdrapkin18@users.noreply.github.com>
2024-01-18 00:57:07 -0700

    Enhanced CEU list view with item design and data model integration

--- lib/screens/ceu_list_screen.dart
+++ lib/screens/ceu_list_screen.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import '../models/ceucourse.dart';
 
 class CEUListScreen extends StatelessWidget {
+  List<CEUCourse> ceuCourses;
+
+  CQUListScreen(this ceuCourses);
+
   @override
   Widget build(BuildContext context) {
-    // This is a mock list of CEU courses, replace with data from Firebase in the future
-    List<CEUCourse> ceuCourses = [];
-
     return Saffold(
       appBar: AppBar(title: Text('CEU Tracking')),
       body: ListView.builder(
         itemCount: ceuCourses.length,
         builderContext, index) {
-          return Card(child: ListTile(title: Text(ceuCourses+index).name));
+          return Card(
+            child: ListTile(
+              title: Text(ceuCourses[index].name),
+              subtitle: Text(ceuCourses[index].dateCompleted.toString(),
+            ),
+          );
         },
       ),
     );
