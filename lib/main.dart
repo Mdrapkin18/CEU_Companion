05e624af3eef08b292deae1d8f87ff36e1e84f4f
Mdrapkin18 <mattdrapkin@gmail.com>
2024-01-18 10:55:02 -0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+
 import 'screens/ceu_add_course_form.dart';
-import 'screens/ceu_detail_screen.dart';
-import 'models/ceucourse.dart';
 import 'screens/ceu_list_screen.dart';
 import 'screens/ceu_progress_tracking_view.dart';
 import 'screens/dashboard_view.dart';
@@ -16,16 +15,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CEU Companion',
       theme: ThemeData(
-        brightness: Brightness.light,
         primarySwatch: Colors.blue,
-        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-        useMaterial3: true,
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<CEUCourse> ceuCourses = []; // Replace with actual data source
+  final double progressValue = 0.5; // Replace with actual progress calculation
+
+  static List<Widget> _widgetOptions(BuildContext context) => <Widget>[
+        DashboardView(),
+        CEUListScreen(), // Assuming CEUListScreen() takes no parameters now
+        CEUAddCourseForm(),
+        CEUProgressTrackingView(progress: progressValue),
+      ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CEU Companion'),
       ),
-      home: DashboardView(),
+      body: Center(
+        child: _widgetOptions(context).elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'CEUs',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Add CEU',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Progress',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
